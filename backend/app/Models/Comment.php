f6af2cefd72b296e77415a8530181dd0d4144109
AbdellahLemtiri <lemtiriabdellah@gmail.com>
2026-02-16 18:08:33 +0000

    feat(models): setup polymorphic relation in Comment model

--- backend/app/Models/Comment.php
+++ backend/app/Models/Comment.php
@@ -3,8 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 class Comment extends Model
 {
     //
+
+    use SoftDeletes;
+    protected $fillable = ['body', 'is_flagged', 'user_id'];
+
+    public function commentable(): MorphTo { return $this->morphTo(); }
+    public function user(): BelongsTo { return $this->belongsTo(User::class); }
 }
