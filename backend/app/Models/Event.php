2ddd4874a72f66b77f124a59ca3707363c3ff405
AbdellahLemtiri <lemtiriabdellah@gmail.com>
2026-02-16 18:08:34 +0000

    feat(models): add relationships to Event model

--- backend/app/Models/Event.php
+++ backend/app/Models/Event.php
@@ -1,10 +1,16 @@
 <?php
 
 namespace App\Models;
-
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\MorphOne;
 class Event extends Model
 {
     //
+
+    protected $guarded = ['id'];
+    protected $casts = ['start_date' => 'datetime', 'end_date' => 'datetime'];
+
+    public function participations(): HasMany { return $this->hasMany(Participation::class); }
+    public function coverImage(): MorphOne { return $this->morphOne(Media::class, 'model'); }
 }
