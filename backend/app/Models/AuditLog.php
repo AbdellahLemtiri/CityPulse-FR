90300c4d18da80d145a914c641f2e3b5713d2265
AbdellahLemtiri <lemtiriabdellah@gmail.com>
2026-02-16 18:12:56 +0000

    feat(models): setup AuditLog tracking relationships

--- backend/app/Models/AuditLog.php
+++ backend/app/Models/AuditLog.php
@@ -3,8 +3,21 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class AuditLog extends Model
 {
     //
+    protected $guarded = ['id'];
+    protected $casts = ['changes' => 'array'];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+    public function subject(): MorphTo
+    {
+        return $this->morphTo('model');
+    }
 }
