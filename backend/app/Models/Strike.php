66d77c43283509ba09289990367ff2471cffa9f2
AbdellahLemtiri <lemtiriabdellah@gmail.com>
2026-02-16 18:12:55 +0000

    feat(models): add relationships to Strike model

--- backend/app/Models/Strike.php
+++ backend/app/Models/Strike.php
@@ -3,8 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Strike extends Model
 {
     //
+    protected $fillable = ['reason', 'user_id'];
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
