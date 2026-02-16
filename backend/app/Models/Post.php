2b99f962d3c08526c3d0ef7c560460a10b91ab5b
AbdellahLemtiri <lemtiriabdellah@gmail.com>
2026-02-16 18:12:54 +0000

    feat(models): add relationships to Post model

--- backend/app/Models/Post.php
+++ backend/app/Models/Post.php
@@ -3,8 +3,29 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
 class Post extends Model
 {
     //
+    protected $fillable = ['caption', 'location', 'is_approved', 'status', 'user_id'];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    // Polymorphic Relations
+    public function media(): MorphMany
+    {
+        return $this->morphMany(Media::class, 'model');
+    }
+    public function comments(): MorphMany
+    {
+        return $this->morphMany(Comment::class, 'commentable');
+    }
+    public function likes(): MorphMany
+    {
+        return $this->morphMany(Like::class, 'likeable');
+    }
 }
