fce2258827041064e4cc43f3e932343e57f1da23
AbdellahLemtiri <lemtiriabdellah@gmail.com>
2026-02-16 18:08:32 +0000

    feat(models): add relationships to Incident model

--- backend/app/Models/Incident.php
+++ backend/app/Models/Incident.php
@@ -2,9 +2,26 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
 class Incident extends Model
 {
     //
+
+    use HasFactory, SoftDeletes;
+    
+    protected $guarded = ['id'];
+
+
+    public function user(): BelongsTo { return $this->belongsTo(User::class); }
+    public function category(): BelongsTo { return $this->belongsTo(Category::class); }
+    public function sector(): BelongsTo { return $this->belongsTo(Sector::class); }
+    public function partner(): BelongsTo { return $this->belongsTo(Partner::class); }
+ 
+    public function media(): MorphMany { return $this->morphMany(Media::class, 'model'); }
+    public function comments(): MorphMany { return $this->morphMany(Comment::class, 'commentable'); }
+    public function likes(): MorphMany { return $this->morphMany(Like::class, 'likeable'); }
 }
