353bca363347fdc3bc2ec43ff6e148ab36ba43a3
AbdellahLemtiri <lemtiriabdellah@gmail.com>
2026-02-16 18:08:32 +0000

    feat(models): add relationships to Category model

--- backend/app/Models/Category.php
+++ backend/app/Models/Category.php
@@ -3,8 +3,18 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Relations\MorphOne;
 
 class Category extends Model
 {
     //
+    protected $fillable = ['name', 'sla_hours'];
+
+    public function incidents(): HasMany { return $this->hasMany(Incident::class); }
+    public function partner(): HasOne { return $this->hasOne(Partner::class); }
+    
+    // Icon (Image)
+    public function image(): MorphOne { return $this->morphOne(Media::class, 'model'); }
 }
