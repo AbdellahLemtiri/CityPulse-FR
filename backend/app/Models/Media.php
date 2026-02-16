9a8a649d79291f3b249ed054e510060edd9d54a0
AbdellahLemtiri <lemtiriabdellah@gmail.com>
2026-02-16 18:08:33 +0000

    feat(models): setup polymorphic relation in Media model

--- backend/app/Models/Media.php
+++ backend/app/Models/Media.php
@@ -3,8 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\Relations\MorphTo;
 class Media extends Model
 {
     //
+    protected $fillable = ['file_path', 'file_type', 'is_public'];
+    
+   
+    public function model(): MorphTo { return $this->morphTo(); }
 }
