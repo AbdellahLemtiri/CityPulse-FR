e634ea957f58211aecfed526d9e151df13a2622f
AbdellahLemtiri <lemtiriabdellah@gmail.com>
2026-02-16 18:08:32 +0000

    feat(models): add relationships to Sector model

--- backend/app/Models/Sector.php
+++ backend/app/Models/Sector.php
@@ -3,8 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\Relations\HasMany;
 class Sector extends Model
 {
     //
+
+    protected $fillable = ['name', 'city', 'boundaries'];
+    protected $casts = ['boundaries' => 'array'];
+    
+    public function users(): HasMany { return $this->hasMany(User::class); }
+    public function incidents(): HasMany { return $this->hasMany(Incident::class); }
 }
