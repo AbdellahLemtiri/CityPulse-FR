6d1420aa1c7d84019f1346d99d457b66a9cfc6cb
AbdellahLemtiri <lemtiriabdellah@gmail.com>
2026-02-21 23:36:49 +0000

    feat: setup axios client interceptors for backend API communication

---
+++ frontend/src/axios-client.js
@@ -0,0 +1,33 @@
+import axios from "axios";
+
+const axiosClient = axios.create({
+  baseURL: `http://127.0.0.1:8000/api` // الرابط الأصلي د API
+});
+
+// 1. قبل ما يمشي الطلب (Request Interceptor)
+// هاد الكود كيلصق التوكن فأي طلب غادي للسيرفر اوتوماتيك
+axiosClient.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+ 
+axiosClient.interceptors.response.use((response) => {
+  return response;
+}, (error) => {
+  try {
+    const { response } = error;
+    if (response.status === 401) {
+      localStorage.removeItem('token');
+       
+    }
+  } catch (e) {
+    console.error(e);
+  }
+  throw error;
+});
+
+export default axiosClient;
