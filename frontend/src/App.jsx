dedfd6435cc49dec2432f2ce1548828dbe4041f7
AbdellahLemtiri <lemtiriabdellah@gmail.com>
2026-03-12 22:58:58 +0000

    feat(frontend): configuration du Routing avec CitoyenLayout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,29 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Welcome from './Welcome';  
 import Login from './Login';
 import Register from './Register'; 
 import Dashboard from './Dashboard';
+import SafiGram from './citoyen/SafiGram';
+import HomeFeed from './citoyen/HomeFeed';
+import CitoyenLayout from './components/layouts/CitoyenLayout';
 
 function App() {
   return (
     <Routes>
+      {/* Routes Publiques */}
       <Route path="/" element={<Welcome />} />  
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} /> 
-      <Route path="/dashboard" element={<Dashboard />} />
+       
+      {/* Route de l'Espace Citoyen (Layout Parent) */}
+     <Route  element={<CitoyenLayout />}>
+        
+        <Route path="/safigram" element={<SafiGram />} />
+        
+        <Route path="/homefeed" element={<HomeFeed />} />
+        {/* <Route path="/profil" element={<Profil />} /> */}
+      </Route>
+
     </Routes>
   );
 }
