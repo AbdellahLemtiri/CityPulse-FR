51a60ec6f0ee17709ef896e82588aa7834e1fcb8
AbdellahLemtiri <lemtiriabdellah@gmail.com>
2026-03-14 13:29:34 +0000

    feat(model): setup polymorphic relations in Comment model

--- backend/app/Models/Comment.php
+++ backend/app/Models/Comment.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
 class Comment extends Model
 {
     //
