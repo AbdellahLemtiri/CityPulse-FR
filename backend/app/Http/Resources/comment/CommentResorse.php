0a27b698c6ea9dab21733c14f9ab54649be11eab
AbdellahLemtiri <lemtiriabdellah@gmail.com>
2026-03-28 16:26:24 +0100

    feat(CommentResource): create CommentResource for structured comment data transformation, including author details and accessibility checks

---
+++ backend/app/Http/Resources/comment/CommentResorse.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources\comment;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+use App\Http\Resources\comment\ReplyCommentResorse;
+
+class CommentResorse extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id'            => $this->id,
+            'body'          => $this->body,
+            'created_at'    => $this->created_at->diffForHumans(),
+            'author_name'   => $this->user->first_name . ' ' . $this->user->last_name,
+            'is_accessible' => $this->user->role_id === 1 || in_array($this->user->id, [2, 4]),
+            'replies'       => ReplyCommentResorse::collection($this->whenLoaded('replies')),
+        ];
+    }
+}
