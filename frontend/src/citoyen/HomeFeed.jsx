da5496543adbecebc46bc9331af4d0585a8136da
AbdellahLemtiri <lemtiriabdellah@gmail.com>
2026-03-28 16:28:15 +0100

    feat(HomeFeed): implement comment fetching and posting functionality with improved UI for comments display

--- frontend/src/citoyen/HomeFeed.jsx
+++ frontend/src/citoyen/HomeFeed.jsx
@@ -2,20 +2,19 @@ import { useState, useEffect } from 'react';
 import axiosClient from '../config/axios-client';
 // import { formatDistanceToNow } from "date-fns";
 // import { fr } from "date-fns/locale";
-
+import CommentItem from './CommentItem.jsx';
 export default function HomeFeed() {
   const [activeTab, setActiveTab] = useState('Tout');
   const [posts, setPosts] = useState([]);
   const [loading, setLoading] = useState(true);
-  const [commentsSection, setCommentsSection] = useState(false);
   const [replyingTo, setReplyingTo] = useState(null);
+   const [openCommentsId, setOpenCommentsId] = useState(null);
   const [commentText, setCommentText] = useState('');
-  const [openCommentsId, setOpenCommentsId] = useState(null);
 
   const [page, setPage] = useState(1);
   const [hasMore, setHasMore] = useState(true);
   const [loadingMore, setLoadingMore] = useState(false);
-  // const [CommentsSection, setCommentsSection] = useState(false);
+  const [comments, setComments] = useState([{}]);
   useEffect(() => {
     setPosts([]);
     setPage(1);
@@ -68,7 +67,6 @@ export default function HomeFeed() {
       setLoadingMore(false);
     }
   };
-
   const handleLoadMore = () => {
     if (!loadingMore && hasMore) {
       fetchFeed(activeTab, page + 1, true);
@@ -102,10 +100,37 @@ export default function HomeFeed() {
     }
   };
 
-  const handleComment = (clickedPost) => {
-    setCommentsSection(true);
+  async function handleFetchComments(postId) {
+    try {
+      if (openCommentsId === postId) {
+        setOpenCommentsId(null);
+      } else {
+        setOpenCommentsId(postId);
+
+        let response = await axiosClient.get(`/comments?commentable_type=Article&commentable_id=${postId}`);
+
+        setComments(response.data.data);
+        console.log(response.data.data);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  async function handleSendComment(postId){
+    try {
+      let data = new FormData();
+      data.append('commentable_type', 'Article');
+      data.append('commentable_id', postId);
+      data.append('body', commentText);
+
+      console.log(commentText+'ddezd');
+      await axiosClient.post('/comments', data);
+      setCommentText('');
+    } catch (error) {
+      console.log(error);
+    }
   };
-  console.log(posts);
+  console.log(comments);
   return (
     <>
       <div className="sticky relative  w-full top-[60px] md:top-[64px] z-30 rounded-b-lg pt-2 pb-2 px-2 md:px-0 mb-4 bg-gray-50 dark:bg-gray-800">
@@ -133,9 +158,8 @@ export default function HomeFeed() {
         ) : (
           <>
             {posts.map((post) => (
-              <div key={post.id} className="bg-white dark:bg-gray-800 md:rounded-lg border-y md:border border-gray-200 dark:border-gray-700 mb-6">
+              <div key={post.id} className="bg-white dark:bg-gray-800 px-1.5 md:rounded-lg border-y md:border border-gray-200 dark:border-gray-700 mb-6">
                 <div className="p-4 flex items-center gap-3">
-                  1
                   <div className="w-10 h-10 rounded-full border border-gray-200 dark:border-gray-700 bg-gray-100 flex items-center justify-center overflow-hidden">
                     <span className="material-symbols-outlined text-gray-500">account_circle</span>
                   </div>
@@ -174,7 +198,7 @@ export default function HomeFeed() {
                     {'  '}
                   </button>
 
-                  <button onClick={() => setOpenCommentsId(openCommentsId === post.id ? null : post.id)} className="flex-1  flex items-center justify-center gap-2 py-1 text-gray-500 dark:text-gray-400 hover:bg-gray-50 dark:hover:bg-gray-700 rounded-lg  group">
+                  <button onClick={() => handleFetchComments(post.id)} className="flex-1  flex items-center justify-center gap-2 py-1 text-gray-500 dark:text-gray-400 hover:bg-gray-50 dark:hover:bg-gray-700 rounded-lg  group">
                     <span className="material-symbols-outlined  ">comment</span>
                     {post.comments_count || 0}
                   </button>
@@ -184,44 +208,24 @@ export default function HomeFeed() {
                   </button>
                 </div>
 
-                 {openCommentsId === post.id && (
+                {openCommentsId === post.id && (
                   <div className="border-t border-gray-100 dark:border-gray-700 bg-gray-50/50 dark:bg-gray-900/20 p-4 animate-fade-in rounded-b-2xl">
                     <div className="space-y-4 mb-4 max-h-60 overflow-y-auto no-scrollbar">
-                      <div className="flex gap-2">
-                        <div className="w-8 h-8 rounded-full bg-gray-300 flex-shrink-0 mt-1"></div>
-                        <div className="flex-1">
-                          <div className="bg-white dark:bg-gray-800 border border-gray-100 dark:border-gray-700 p-2.5 rounded-2xl rounded-tl-none shadow-sm">
-                            <div className="flex justify-between items-center">
-                              <h4 className="font-bold text-xs text-gray-900 dark:text-gray-100">Brahim El Filali</h4>
-                              <span className="text-[10px] text-gray-400">Il y a 10 min</span>
-                            </div>
-                            <p className="text-sm text-gray-700 dark:text-gray-300 mt-0.5">Mzyan tbarkellah, walakin wach hadchi ghadi y-koun f ga3 l-a7ya2?</p>
-                          </div>
-                          <button className="text-[11px] font-bold text-gray-500 hover:text-blue-600 mt-1 ml-2 transition-colors">Répondre</button>
-                        </div>
-                      </div>
-
-                      <div className="flex gap-2 ml-8">
-                        <div className="w-6 h-6 rounded-full bg-blue-100 flex items-center justify-center flex-shrink-0 mt-1">
-                          <span className="material-symbols-outlined text-[12px] text-blue-600">verified_user</span>
-                        </div>
-                        <div className="flex-1">
-                          <div className="bg-blue-50 dark:bg-blue-900/20 p-2 rounded-xl rounded-tl-none border border-blue-100 dark:border-blue-800">
-                            <h4 className="font-bold text-xs text-blue-800 dark:text-blue-300 flex items-center gap-1">
-                              Admin SafiPulse
-                              <span className="bg-blue-600 text-white text-[8px] px-1 rounded-sm">OFFICIEL</span>
-                            </h4>
-                            <p className="text-xs text-blue-700 dark:text-blue-200 mt-0.5">Ah b-tabi3at l-7al a si Brahim, l-projet ghadi y-chmel mdint Asfi kamla.</p>
-                          </div>
-                        </div>
+                      <div className="space-y-4 mb-4 max-h-60 overflow-y-auto no-scrollbar">
+                        {comments.length === 0 ? (
+                          <div className="text-center text-gray-500 py-4 text-sm">Aucun commentaire pour l'instant. Soyez le premier !</div>
+                        ) : (
+                          comments.map((comment) => (
+                          <CommentItem key={comment.id} comment={comment} />
+                          ))
+                        )}
                       </div>
                     </div>
 
-                    {/* INPUT AJOUTER UN COMMENTAIRE */}
                     <div className="flex gap-2 items-center mt-2 relative">
                       <div className="w-8 h-8 rounded-full bg-gray-300 flex-shrink-0"></div>
-                      <input type="text" placeholder="Écrire un commentaire..." className="flex-1 bg-white dark:bg-gray-800 border border-gray-200 dark:border-gray-700 rounded-full px-4 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-blue-500/50 shadow-sm" />
-                      <button className="absolute right-2 top-1/2 -translate-y-1/2 text-blue-600 hover:text-blue-800 w-8 h-8 flex items-center justify-center rounded-full transition-colors">
+                      <input value={commentText} onChange={ (e) => setCommentText(e.target.value)}  type="text" placeholder="Écrire un commentaire..." className="flex-1 bg-white dark:bg-gray-800 border border-gray-200 dark:border-gray-700 rounded-full px-4 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-blue-500/50 shadow-sm" />
+                      <button onClick={() => handleSendComment(openCommentsId)} className="absolute right-2 top-1/2 -translate-y-1/2 text-blue-600 hover:text-blue-800 w-8 h-8 flex items-center justify-center rounded-full transition-colors">
                         <span className="material-symbols-outlined text-[20px]">send</span>
                       </button>
                     </div>
