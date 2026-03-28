d161d272865dcba2cfee7186f7c9587dac0ac770
AbdellahLemtiri <lemtiriabdellah@gmail.com>
2026-03-28 16:26:33 +0100

    feat(ReplyCommentResource): add ReplyCommentResource for structured reply comment data transformation, including author details and accessibility checks

---
+++ backend/app/Http/Resources/comment/ReplyCommentResorse.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources\comment;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ReplyCommentResorse extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+
+    public function toArray(Request $request): array
+    {
+        return [
+            'id'            => $this->id, // 👈 Zdnaha
+            'body'          => $this->body, // 👈 S77naha
+            'created_at'    => $this->created_at->diffForHumans(),
+            'author_name'   => $this->user->first_name . ' ' . $this->user->last_name, 
+             'is_accessible' => $this->user->role_id === 1 || in_array($this->user->id, [2, 4]),
+        ];
+    }
+}
