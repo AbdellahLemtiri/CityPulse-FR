6638bd2d09cebe5c91410fc981d8b4210c5b2c56
AbdellahLemtiri <lemtiriabdellah@gmail.com>
2026-04-02 11:04:00 +0100

    refactor(CommentController): remove unused methods and clean up code for improved readability

--- backend/app/Http/Controllers/CommentController.php
+++ backend/app/Http/Controllers/CommentController.php
@@ -17,6 +17,7 @@ class CommentController extends Controller
      */
     public function index(getCommentRequest $request)
     {
+        //
 
         $data = $request->validated();
 
@@ -31,16 +32,7 @@ class CommentController extends Controller
             }
         ])->latest()->paginate(10);
 
-
         return CommentResorse::collection($comments);
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
     }
 
     /**
@@ -67,30 +59,7 @@ class CommentController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
-    {
-        //
-    }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(Comment $comment)
     {
         $UserId = Auth::id();
