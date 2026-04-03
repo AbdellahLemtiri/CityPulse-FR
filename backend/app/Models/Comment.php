19cf1a6bc940a582debc5e6e6d3b0e119e2776b1
AbdellahLemtiri <lemtiriabdellah@gmail.com>
2026-04-03 19:43:10 +0100

    feat(CommentModel): add parent_id to fillable attributes and enhance replies method with user data and ordering

--- backend/app/Models/Comment.php
+++ backend/app/Models/Comment.php
@@ -7,22 +7,31 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+
 class Comment extends Model
 {
     //
 
-   use SoftDeletes;
-    protected $fillable = ['body', 'is_flagged', 'user_id','deleted_at'];
+    use SoftDeletes;
+    protected $fillable = ['body', 'is_flagged', 'user_id', 'deleted_at', 'parent_id'];
 
-    public function commentable(): MorphTo { return $this->morphTo(); }
-    public function user(): BelongsTo { return $this->belongsTo(User::class); }
+    public function commentable(): MorphTo
+    {
+        return $this->morphTo();
+    }
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
     public function parent(): BelongsTo
     {
         return $this->belongsTo(Comment::class, 'parent_id');
     }
 
-     public function replies(): HasMany
+    public function replies(): HasMany
     {
-        return $this->hasMany(Comment::class, 'parent_id')->with('replies');
+        return $this->hasMany(Comment::class, 'parent_id')
+            ->with(['user:id,first_name,last_name,role_id', 'replies'])
+            ->orderBy('created_at', 'asc');
     }
 }
