9a5efc9c2d67590deee0e29eae3c4a07b196ce2b
AbdellahLemtiri <lemtiriabdellah@gmail.com>
2026-04-03 23:45:03 +0100

    feat(UserResource): create UserResource class to transform user data into a structured JSON response

---
+++ backend/app/Http/Resources/UserResource.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'uuid' => $this->uuid,
+            'first_name' => $this->first_name,
+            'last_name'  => $this->last_name,
+            'full_name'  => $this->first_name . ' ' . $this->last_name,
+            'email' => $this->email,
+            'phone' => $this->phone,
+            'cin' => $this->cin,
+            'adresse' => $this->adresse,
+            'role' => $this->role->name,
+            'city' => $this->city->name,
+            'sector' => $this->sector->name,
+            'joined_at'  => $this->created_at->diffForHumans(),
+        ];
+    }
+}
