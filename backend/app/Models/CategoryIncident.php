16bc7fca98d002b5c2beb1190692fba56f52e44e
AbdellahLemtiri <lemtiriabdellah@gmail.com>
2026-04-03 19:42:42 +0100

    refactor(CategoryIncidentModel): update partners relationship to use BelongsToMany and remove partner_id from fillable attributes

--- backend/app/Models/CategoryIncident.php
+++ backend/app/Models/CategoryIncident.php
@@ -6,21 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
- 
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class CategoryIncident extends Model
 {
     protected $table = 'category_incidents';
-    protected $fillable = ['name', 'sla_hours', 'is_active','description','partner_id'];
+    protected $fillable = ['name', 'sla_hours', 'is_active', 'description'];
 
     public function incidents(): HasMany
     {
         return $this->hasMany(Incident::class, 'category_id');
     }
 
-    public function partner(): BelongsTo
+    public function partners(): BelongsToMany
     {
-        return $this->belongsTo(Partner::class);
+        return $this->belongsToMany(Partner::class, 'category_partner');
     }
-
 }
