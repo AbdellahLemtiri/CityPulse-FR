f028acfb407d14b12a38f75cb25b369b6889acc5
AbdellahLemtiri <lemtiriabdellah@gmail.com>
2026-04-03 19:43:37 +0100

    feat(PartnerModel): add city_id to fillable attributes and update categories relationship to BelongsToMany; introduce city relationship

--- backend/app/Models/Partner.php
+++ backend/app/Models/Partner.php
@@ -6,18 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\MorphOne;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Partner extends Model
 {
-    protected $fillable = ['name', 'email', 'phone_fix', 'whatsapp', 'sla_hours'];
+    protected $fillable = ['name', 'email', 'phone_fix', 'whatsapp', 'sla_hours', 'city_id'];
 
-    public function categories(): HasMany
+    public function categories(): BelongsToMany
     {
-        return $this->hasMany(
-            CategoryIncident::class
-        );
+        return $this->belongsToMany(CategoryIncident::class, 'category_partner');
     }
 
+    public function city(): BelongsTo
+    {
+        return $this->belongsTo(City::class, 'city_id');
+    }
 
 
     public function incidents(): HasMany
@@ -25,7 +28,7 @@ class Partner extends Model
         return $this->hasMany(Incident::class, 'partner_id');
     }
 
-     public function logo(): MorphOne
+    public function logo(): MorphOne
     {
         return $this->morphOne(Media::class, 'model');
     }
