3ef2a320c7349937b2d9a34273be3d1298a26630
AbdellahLemtiri <lemtiriabdellah@gmail.com>
2026-04-03 23:43:53 +0100

    feat(AuthController): enhance user registration and login by incorporating sector and role relationships, and return user data as a resource

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -12,36 +12,40 @@ use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Auth;
 use Laravel\Sanctum\HasApiTokens;
+use App\Models\Sector;
+use App\Models\Role;
+use App\Http\Resources\UserResource;
 class AuthController extends Controller
 {
     //
 
-  public function register(registerRequest $request)
+    public function register(registerRequest $request)
     {
-         
-        $data = $request->validated(); 
- 
+
+        $data = $request->validated();
+
+        $sector = Sector::find($data['sector_id']);
         $user = User::create([
-            'uuid' => Str::uuid(),  
+            'uuid' => Str::uuid(),
             'first_name' => $data['first_name'],
             'last_name' => $data['last_name'],
             'email' => $data['email'],
-            'password' => Hash::make($data['password']),  
-            'role_id' => 3,
-            'sector_id' => $data['sector_id'], 
-            'phone' => $data['telephone'], 
+            'password' => Hash::make($data['password']),
+            'role_id' => Role::where('name', 'citoyen')->first()->id,
+            'sector_id' => $sector->id,
+            'phone' => $data['telephone'],
             'cin' => $data['cin'],
             'adresse' => $data['adresse'] ?? null,
+            'city_id' => $sector->city_id
         ]);
 
-       
         $token = $user->createToken('auth_token')->plainTextToken;
 
-        $user =$user->load('role')->load('sector')->get();    
+        $user->load(['role', 'sector', 'city']);
         return response()->json([
             'message' => 'Compte créé avec succès',
             'access_token' => $token,
-            'user' => $user,
+            'user' => new UserResource($user),
         ], 201);
     }
 
@@ -55,12 +59,11 @@ class AuthController extends Controller
 
         $user = User::where('email', $data['email'])->firstOrFail();
         $token = $user->createToken('auth_token')->plainTextToken;
-
-      $user->load('role')->load('sector')->get();
+        $user->load(['role', 'sector', 'city']);
         return response()->json([
             'message' => 'Connexion réussie',
             'access_token' => $token,
-            'user' => $user,
+            'user' => new UserResource($user),
         ]);
     }
 }
