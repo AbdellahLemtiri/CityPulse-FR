cd7366d3c0c21efc92939810e8bdac1914dcb3dc
AbdellahLemtiri <lemtiriabdellah@gmail.com>
2026-04-03 19:42:07 +0100

    feat(ReplyCommentResorse): add replies collection to array return in toArray method

--- backend/app/Http/Resources/comment/ReplyCommentResorse.php
+++ backend/app/Http/Resources/comment/ReplyCommentResorse.php
@@ -16,11 +16,12 @@ class ReplyCommentResorse extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            'id'            => $this->id,  
-            'body'          => $this->body,  
+            'id'            => $this->id,
+            'body'          => $this->body,
             'created_at'    => $this->created_at->diffForHumans(),
-            'author_name'   => $this->user->first_name . ' ' . $this->user->last_name, 
-             'is_accessible' => $this->user->role_id === 1 || in_array($this->user->id, [2, 4]),
+            'author_name'   => $this->user->first_name . ' ' . $this->user->last_name,
+            'is_accessible' => $this->user->role_id === 1 || in_array($this->user->id, [2, 4]),
+            'replies' => ReplyCommentResorse::collection($this->whenLoaded('replies')),
         ];
     }
 }
