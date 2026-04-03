cf29af32489cf09972b399bceab390ce20391573
AbdellahLemtiri <lemtiriabdellah@gmail.com>
2026-04-03 19:41:56 +0100

    refactor(CommentResorse): standardize formatting of array return in toArray method

--- backend/app/Http/Resources/comment/CommentResorse.php
+++ backend/app/Http/Resources/comment/CommentResorse.php
@@ -16,12 +16,12 @@ class CommentResorse extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            'id'            => $this->id,
-            'body'          => $this->body,
-            'created_at'    => $this->created_at->diffForHumans(),
-            'author_name'   => $this->user->first_name . ' ' . $this->user->last_name,
+            'id' => $this->id,
+            'body' => $this->body,
+            'created_at' => $this->created_at->diffForHumans(),
+            'author_name' => $this->user->first_name . ' ' . $this->user->last_name,
             'is_accessible' => $this->user->role_id === 1 || in_array($this->user->id, [2, 4]),
-            'replies'       => ReplyCommentResorse::collection($this->whenLoaded('replies')),
-        ];
+            'replies' => ReplyCommentResorse::collection($this->whenLoaded('replies')),         ];
     }
+    
 }
