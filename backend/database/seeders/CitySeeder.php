60d8b6188278734ddebf3943a7e4ae17062c0d7e
AbdellahLemtiri <lemtiriabdellah@gmail.com>
2026-04-04 13:27:58 +0100

    feat(CitySeeder): add seeder for cities to populate database with initial city data

---
+++ backend/database/seeders/CitySeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\City;
+class CitySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+
+        $cities = [
+            ['name' => 'Safi'],
+            ['name' => 'Casablanca'],
+            ['name' => 'Marrakech'],
+        ];
+
+        foreach ($cities as $city) {
+            City::firstOrCreate(['name' => $city['name']]);
+        }
+    }
+}
