d6af73f379b6e1f4af6c9099422e892489496e7b
AbdellahLemtiri <lemtiriabdellah@gmail.com>
2026-04-06 19:47:38 +0100

    feat(ModerationController): implement user search functionality with validation

--- backend/app/Http/Controllers/ModerationController.php
+++ backend/app/Http/Controllers/ModerationController.php
@@ -4,8 +4,19 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
+use App\Models\User;
+use App\Http\Requests\users\searchUsers;
 class ModerationController extends Controller
 {
     //
+
+    public function SerchUser(searchUsers $request){
+        
+        $data = $request->validated();
+        $users = User::where('last_name', 'like', '%' . $data['search'] . '%')->orWhere('email', 'like', '%' . $data['search'] . '%')->Orwhere('cin', 'like', '%' . $data['search'] . '%')->orWhere('first_name', 'like', '%' . $data['search'] . '%')->get();
+        return response()->json($users, 200);
+    }
+    
+    
+    //users
 }
