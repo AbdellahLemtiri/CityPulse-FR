8e423fc129adc535ff4ad01e4e8b9551f3477078
AbdellahLemtiri <lemtiriabdellah@gmail.com>
2026-04-07 14:00:36 +0100

    feat(ModerationController): enhance user search and implement strike user functionality with validation

--- backend/app/Http/Controllers/ModerationController.php
+++ backend/app/Http/Controllers/ModerationController.php
@@ -6,17 +6,52 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
 use App\Http\Requests\users\searchUsers;
+use App\Http\Requests\users\strikUserRequest;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Role;
+use  App\Http\Resources\ModirationUserResource;
 class ModerationController extends Controller
 {
     //
 
-    public function SerchUser(searchUsers $request){
-        
+    public function SerchUser(searchUsers $request)
+    {
+        $sectorId = Auth::user()->sector_id;
+        $roleId = Role::where('name', 'citoyen')->first()->id;
+
         $data = $request->validated();
-        $users = User::where('last_name', 'like', '%' . $data['search'] . '%')->orWhere('email', 'like', '%' . $data['search'] . '%')->Orwhere('cin', 'like', '%' . $data['search'] . '%')->orWhere('first_name', 'like', '%' . $data['search'] . '%')->get();
+
+        $users = User::where('sector_id', $sectorId)
+            ->where('role_id', $roleId)
+            ->where(function ($query) use ($data) {
+                $query->where('last_name', 'like', '%' . $data['search'] . '%')
+                    ->orWhere('email', 'like', '%' . $data['search'] . '%')
+                    ->orWhere('cin', 'like', '%' . $data['search'] . '%');
+            })
+            ->with('strikes')
+            ->get();
+
+            $users = ModirationUserResource::collection($users);
         return response()->json($users, 200);
     }
-    
-    
-    //users
+
+
+  public function strikeUser(strikUserRequest $request)
+{
+    $data = $request->validated();
+
+     $user = User::with('strikes')->findOrFail($data['id']);
+
+     $user->strikes()->create([
+        'reason' => $data['reason'],
+      
+    ]);
+
+   
+    if ($user->strikes()->count() >= 3) {
+        $user->update(['is_banned' => true]);
+    }
+
+     return response()->json(new ModirationUserResource($user), 200);
+}
 }
