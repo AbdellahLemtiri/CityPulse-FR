a3be73d55c6114b50402e83b71bf726794edab85
AbdellahLemtiri <lemtiriabdellah@gmail.com>
2026-04-08 18:18:46 +0100

    feat(IncidentUpdatedNotification): create notification class for broadcasting incident updates

---
+++ backend/app/Notifications/IncidentUpdatedNotification.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Notifications\Messages\BroadcastMessage;
+use Illuminate\Notifications\Notification;
+
+// 💡 Zid implements ShouldBroadcast
+class IncidentUpdatedNotification extends Notification implements ShouldBroadcast
+{
+    use Queueable;
+
+    public $incidentData;
+
+    public function __construct($incidentData)
+    {
+        $this->incidentData = $incidentData;
+    }
+
+    public function via(object $notifiable): array
+    {
+
+        return ['database', 'broadcast'];
+    }
+
+    public function toArray(object $notifiable): array
+    {
+        return [
+            'incident_id' => $this->incidentData['id'],
+            'title' => $this->incidentData['title'],
+            'message' => 'Votre incident a été mis à jour.',
+        ];
+    }
+
+    public function toBroadcast(object $notifiable): BroadcastMessage
+    {
+        return new BroadcastMessage([
+            'incident_id' => $this->incidentData['id'],
+            'title' => $this->incidentData['title'],
+            'message' => 'Votre incident a été mis à jour.',
+        ]);
+    }
+}
