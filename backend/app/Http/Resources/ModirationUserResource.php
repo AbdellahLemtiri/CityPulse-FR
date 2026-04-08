a6eaed38127a9d1f160c2bd227023339ec0d3a46
AbdellahLemtiri <lemtiriabdellah@gmail.com>
2026-04-08 18:17:04 +0100

    feat(ModirationUserResource): create resource for user moderation with detailed user information

---
+++ backend/app/Http/Resources/ModirationUserResource.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ModirationUserResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'first_name' => $this->first_name,
+            'last_name' => $this->last_name,
+            'email' => $this->email,
+            'phone' => $this->phone,
+            'is_banned' => $this->is_banned,
+            'cin' => $this->cin,
+            'strikes_count'=>$this->strikes()->count()??0,
+            'created_at'=> $this->created_at->diffForHumans(),      
+        ];
+    }
+}
