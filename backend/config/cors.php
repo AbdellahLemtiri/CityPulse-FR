52b850eb8407f20855846020688e3780ca08dca6
AbdellahLemtiri <lemtiriabdellah@gmail.com>
2026-04-08 18:20:24 +0100

    feat(database): enhance user and article models with new fields, update migrations, and add foreign key constraints

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -15,8 +15,7 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
-
+    'paths' => ['api/*', 'sanctum/csrf-cookie', 'broadcasting/auth'],
     'allowed_methods' => ['*'],
 
     'allowed_origins' => ['http://localhost:5173', 'http://127.0.0.1:5173'],
