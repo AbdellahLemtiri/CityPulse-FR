d94a51980a0e37d5f99d5af25b36fcf7749d37cb
AbdellahLemtiri <lemtiriabdellah@gmail.com>
2026-04-08 18:15:02 +0100

    feat(ProfileController): create ProfileController to handle user profile retrieval

---
+++ backend/app/Http/Controllers/ProfileController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class ProfileController extends Controller
+{
+    //
+    public function index()
+    {
+        //
+        $user = Auth::user();
+        $user->load('role');
+        return response()->json($user, 200);
+    }
+}
