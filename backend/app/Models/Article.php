9cc95bfc164a07e24029dde86dec6e5d25c76c0d
AbdellahLemtiri <lemtiriabdellah@gmail.com>
2026-04-11 17:35:14 +0100

    feat(article): implement soft deletes and slug generation in Article model, add city relationship

--- backend/app/Models/Article.php
+++ backend/app/Models/Article.php
@@ -6,23 +6,27 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
-// use Illuminate\Database\Eloquent\SoftDeletes;
-
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
+use Illuminate\Support\Str;
 
 class Article extends Model
 {
-    /** @use HasFactory<\Database\Factories\ArticleFactory> */
-    // use SoftDeletes, HasFactory;
+    use HasFactory, SoftDeletes, HasSlug;
 
     protected $fillable = [
         'user_id',
-        'sector_id', 
+        'sector_id',
         'slug',
         'content',
         'scope',
         'status',
-        'deleted_at'
+        'city_id',
     ];
+
+
+
 
     public function user(): BelongsTo
     {
@@ -47,5 +51,31 @@ class Article extends Model
     public function sector(): BelongsTo
     {
         return $this->belongsTo(Sector::class);
+    }
+
+    public function city(): BelongsTo
+    {
+        return $this->belongsTo(City::class);
+    }
+
+
+
+    public function getSlugSourceAttribute()
+    {
+        $shortContent = Str::limit($this->content, 50, '');
+        return $shortContent . '-' . Str::random(5);
+    }
+
+    public function getSlugOptions(): SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('slug_source')
+            ->saveSlugsTo('slug')
+            ->doNotGenerateSlugsOnUpdate();
+    }
+
+    public function getRouteKeyName()
+    {
+        return 'slug';
     }
 }
