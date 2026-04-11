71db0938aad07b4d778e5d741d923aeea1cb11d5
AbdellahLemtiri <lemtiriabdellah@gmail.com>
2026-04-11 17:26:51 +0100

    refactor(moderation): streamline user search and strike handling in ModerationController for improved readability and performance

--- backend/app/Http/Controllers/ModerationController.php
+++ backend/app/Http/Controllers/ModerationController.php
@@ -8,21 +8,21 @@ use App\Models\User;
 use App\Http\Requests\users\searchUsers;
 use App\Http\Requests\users\strikUserRequest;
 use Illuminate\Support\Facades\Auth;
-use App\Models\Role;
-use  App\Http\Resources\ModirationUserResource;
+ use  App\Http\Resources\ModirationUserResource;
+
 class ModerationController extends Controller
 {
     //
 
     public function SerchUser(searchUsers $request)
     {
-        $sectorId = Auth::user()->sector_id;
-        $roleId = Role::where('name', 'citoyen')->first()->id;
-
+        $user = Auth::user();
+        $sectorId = $user->sector_id;
         $data = $request->validated();
 
-        $users = User::where('sector_id', $sectorId)
-            ->where('role_id', $roleId)
+        
+        $users = User::role('citoyen') 
+            ->where('sector_id', $sectorId)
             ->where(function ($query) use ($data) {
                 $query->where('last_name', 'like', '%' . $data['search'] . '%')
                     ->orWhere('email', 'like', '%' . $data['search'] . '%')
@@ -31,27 +31,26 @@ class ModerationController extends Controller
             ->with('strikes')
             ->get();
 
-            $users = ModirationUserResource::collection($users);
-        return response()->json($users, 200);
+        return response()->json(ModirationUserResource::collection($users), 200);
     }
 
 
-  public function strikeUser(strikUserRequest $request)
-{
-    $data = $request->validated();
+    public function strikeUser(strikUserRequest $request)
+    {
+        $data = $request->validated();
 
-     $user = User::with('strikes')->findOrFail($data['id']);
+        $user = User::with('strikes')->findOrFail($data['id']);
 
-     $user->strikes()->create([
-        'reason' => $data['reason'],
-      
-    ]);
+        $user->strikes()->create([
+            'reason' => $data['reason'],
 
-   
-    if ($user->strikes()->count() >= 3) {
-        $user->update(['is_banned' => true]);
+        ]);
+
+
+        if ($user->strikes()->count() >= 3) {
+            $user->update(['is_banned' => true]);
+        }
+
+        return response()->json(new ModirationUserResource($user), 200);
     }
-
-     return response()->json(new ModirationUserResource($user), 200);
-}
 }
