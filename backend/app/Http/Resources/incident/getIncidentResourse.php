839a2c2831506cd78f39db85d012571211712738
AbdellahLemtiri <lemtiriabdellah@gmail.com>
2026-04-14 14:49:49 +0100

    feat(resource): enhance getIncidentResource to include audio and image file handling, update date format, and fix reference number retrieval

--- backend/app/Http/Resources/incident/getIncidentResourse.php
+++ backend/app/Http/Resources/incident/getIncidentResourse.php
@@ -4,6 +4,8 @@ namespace App\Http\Resources\incident;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 
 class getIncidentResourse extends JsonResource
 {
@@ -12,19 +14,24 @@ class getIncidentResourse extends JsonResource
      *
      * @return array<string, mixed>
      */
+
     public function toArray(Request $request): array
     {
+        $audio = $this->media->firstWhere('file_type', 'audio');
+        $images = $this->media->where('file_type', 'image');
         return [
             'id' => $this->id,
-            'ref_num' => $this->id,
+            'ref_num' => $this->ref_num,
             'title' => $this->title,
             'description' => $this->description,
-            'created_at' => $this->created_at->diffForHumans(),
+            'created_at' => date('Y-m-d  ', strtotime($this->created_at)),
             'status' => $this->status,
-            'images' => $this->media->pluck('file_path'),
+            'images' => $images->map(fn($m) => asset('storage/' . $m->file_path))->values(),
+            'audio' => $audio ? asset('storage/' . $audio->file_path) : null,
             'address' => $this->location_name,
             'rejection_reason' => $this->location_address,
             'category' => $this->category?->name,
+            'rejection_reason' => $this->rejection_reason ?? null,
         ];
     }
 }
