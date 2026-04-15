629f78a76b2e057914547db7342ad7287207423d
AbdellahLemtiri <lemtiriabdellah@gmail.com>
2026-04-15 11:27:07 +0100

    feat(dutyPharmacy): add authorization checks for create, update, and delete actions in DutyPharmacyController

--- backend/app/Http/Controllers/DutyPharmacyController.php
+++ backend/app/Http/Controllers/DutyPharmacyController.php
@@ -7,13 +7,13 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Requests\StoreDutyPharmacyRequest;
 use App\Http\Requests\UpdateDutyPharmacyRequest;
+use Illuminate\Support\Facades\Gate;
 
 class DutyPharmacyController extends Controller
 {
     public function index(Request $request)
     {
         $cityId =  Auth::user()->city_id;
-
         $pharmacies = DutyPharmacy::where('city_id', $cityId)
             ->where('start_date', '<=', now())
             ->where('end_date', '>=', now())
@@ -27,20 +27,21 @@ class DutyPharmacyController extends Controller
     public function journalistIndex(Request $request)
     {
         $user = Auth::user();
-        $search = $request->query('search'); 
+        $search = $request->query('search');
 
         $pharmacies = DutyPharmacy::where('city_id', $user->city_id)
-             ->when($search, function ($query, $search) {
+            ->when($search, function ($query, $search) {
                 return $query->where('name', 'like', "%{$search}%");
             })
             ->orderBy('name', 'asc')
-            ->paginate(10);  
+            ->paginate(10);
 
         return response()->json($pharmacies);
     }
 
     public function store(StoreDutyPharmacyRequest $request)
     {
+        Gate::authorize('create');
         $data = $request->validated();
         $user = Auth::user();
         $data['city_id'] = $user->city_id;
@@ -49,20 +50,29 @@ class DutyPharmacyController extends Controller
         return response()->json(['message' => 'Pharmacie programmée avec succès', 'data' => $pharmacy], 201);
     }
 
+
     public function show(DutyPharmacy $dutyPharmacy)
     {
         return response()->json($dutyPharmacy);
     }
-    public function update(UpdateDutyPharmacyRequest $request, DutyPharmacy $dutyPharmacy)
+
+
+
+
+     public function update(UpdateDutyPharmacyRequest $request, DutyPharmacy $dutyPharmacy)
     {
+        Gate::authorize('update', $dutyPharmacy);
         $user = Auth::user();
         $data = $request->validated();
         $dutyPharmacy->update($data);
         return response()->json(['message' => 'Pharmacie mise à jour avec succès', 'data' => $dutyPharmacy]);
     }
 
-    public function destroy(DutyPharmacy $dutyPharmacy)
+
+    
+     public function destroy(DutyPharmacy $dutyPharmacy)
     {
+        Gate::authorize('delete', $dutyPharmacy);
         $user = Auth::user();
         if ($dutyPharmacy->city_id !== $user->city_id) {
             return response()->json(['message' => 'Non autorisé'], 403);
