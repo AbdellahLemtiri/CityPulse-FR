8354006efac53a400ab31b7a8fde8b5f2682e869
AbdellahLemtiri <lemtiriabdellah@gmail.com>
2026-04-19 03:44:36 +0100

    chore(env): update environment configuration for CityPulse, add Laravel Echo and Pusher dependencies, and remove deprecated CategoryController

---
+++ backend/tests/pr.js
@@ -0,0 +1,74 @@
+// const students = [
+//   { id: 1, name: "Ali", score: 12, status: "en cours" },
+//   { id: 2, name: "Sara", score: 16, status: "validé" },
+//   { id: 3, name: "Omar", score: 8, status: "rattrapage" },
+//   { id: 4, name: "Aida", score: 14, status: "validé" }
+// ];
+
+// for(let i  = 0 ;i<students.len)
+
+// const chaine = 'helo';
+// let chainII = '';
+// for(let i = chaine.length-1 ; i >=0 ; i--)
+// {
+//   chainII += chaine[i];
+
+// }
+// console.log(chainII);
+
+// const c = "Le développement web est fascinant";
+// let maxletr = "";
+// let maxint = 0;
+// for (let i = 0; i < c.length; i++)
+//   {
+//     let compteur = 0;
+//     for(let j= 0 ;j<c.length ;j++)
+//     {
+//         if(c[i] === c[j])
+//         {
+//           compteur += 1;
+//         }
+//     }
+
+//     if(compteur > maxint )
+//     {
+//       maxint = compteur;
+//       maxletr = c[i];
+//     }
+
+//   }
+//   console.log(maxint);
+//   console.log(maxletr);
+
+// let inpute = "aaabbbdbaaaaa";
+
+// const c = "aaabbbdbaaaaa";
+// let maxletr = "";
+// let maxint = 0;
+// for (let i = 0; i < c.length; i++) {
+//     let compteur = 0;
+//     for (let j = 0; j < c.length; j++) {
+//         if (c[i] === c[j]) {
+//             compteur += 1;
+//         }
+//     }
+
+//     if (compteur > maxint) {
+//         maxint = compteur;
+//         maxletr = c[i];
+//     }
+// }
+// let n = c.length;
+// console.log(n);
+
+// let cmt = 0;
+// for (let i = 0; i < n; i++) {
+//     if (c[i] === maxletr) {
+//         c[i] = c[i+1];
+//         cmt += 1;
+//     }
+// }
+
+
+
+let c = "aaaaaaavcceeedsqds";
---
+++ backend/tests/tempCodeRunnerFile.js
@@ -0,0 +1,9 @@
+
+const chaine = 'helo';
+let chainII = '';
+for(let i = chaine.lenght-1 ; i >=0 ; i--)
+{
+  chainII =+ chaine[i];
+
+}
+console.log(chainII);
