b2793aa3290855c80b7a9f74d2a81fb472a2d8ec
AbdellahLemtiri <lemtiriabdellah@gmail.com>
2026-04-19 20:59:33 +0100

    feat(diagram): update class diagram in draw.io format; adjust cell positions and styles for improved layout; add dayjs dependency in package-lock.json for date handling

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,20 +1,18 @@
 // src/main.jsx
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'   
-import App from './App'
-import { ContextProvider } from './contexts/ContextProvider'
-import { ThemeProvider } from './contexts/ThemeContext' 
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import { ContextProvider } from './contexts/ContextProvider';
+import { ThemeProvider } from './contexts/ThemeContext';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <ContextProvider>
-      <ThemeProvider>  
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </ThemeProvider>
-    </ContextProvider>
-  </React.StrictMode>,
-)
+  <ContextProvider>
+    <ThemeProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
+  </ContextProvider>,
+);
